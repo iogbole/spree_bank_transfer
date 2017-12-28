f8209931981545a701fe6cdbe920f52e55103bf2
Israel Ogbole <iogbole@yahoo.com>
2017-12-28 23:09:40 +0000

    Update 20130717071443_create_table_bank.rb

--- db/migrate/20130717071443_create_table_bank.rb
+++ db/migrate/20130717071443_create_table_bank.rb
@@ -1,4 +1,4 @@
-class CreateTableBank < ActiveRecord::Migration[5.1]
+class CreateTableBank < ActiveRecord::Migration[5.1.4]
   def change
     create_table :spree_banks do |t|
       t.string :name
